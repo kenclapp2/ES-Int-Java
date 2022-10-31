a92b8f1d56d204dd4d313dc9fad6cfdf2f8d6bc4
simon <simon@dancingcloudservices.com>
2022-10-31 11:10:38 -0600

    Initial

---
+++ src/main/java/verify/Verify.java
@@ -0,0 +1,14 @@
+package verify;
+
+record Message(String msg) {}
+
+public class Verify {
+  public static void main(String[] args) {
+    var m = new Message("Hello world!");
+    switch (m) {
+      case Message(String txt) when txt.length() > 3 ->
+        System.out.println(txt);
+      default -> throw new IllegalArgumentException();
+    }
+  }
+}
