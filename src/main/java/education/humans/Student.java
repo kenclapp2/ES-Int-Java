453d8dedb193a6c29875fed87303038b57ef0c29
simon <simon@dancingcloudservices.com>
2022-11-01 11:31:41 -0600

    Initial

---
+++ src/main/java/education/humans/Student.java
@@ -0,0 +1,17 @@
+package education.humans;
+
+public class Student {
+  private String name;
+  private double gpa;
+  /* many courses... each is String ??? */
+
+}
+
+//class Date {
+//  private int day, month, year;
+//
+//  set day of month(d)_{
+//    if (d with month and year is ok)
+//      change day to d.
+//  }
+//}
--- src/main/java/verify/Verify.java
+++ src/main/java/verify/Verify.java
@@ -1,13 +1,13 @@
 package verify;
 
-record Message(String msg) {}
+record Message(String msg) {
+}
 
 public class Verify {
   public static void main(String[] args) {
     var m = new Message("Hello world!");
     switch (m) {
-      case Message(String txt) when txt.length() > 3 ->
-        System.out.println(txt);
+      case Message(String txt)when txt.length() > 3 -> System.out.println(txt);
       default -> throw new IllegalArgumentException();
     }
   }
